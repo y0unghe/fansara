00ffffaaa2c6dee7b5cf61b502531a69c7bf3547
yong <yong.dev@outlook.com>
2022-10-13 17:50:35 +0800

    show user profile image

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import SidebarLink from './SidebarLink'
 import { BellIcon, UserCircleIcon, ChatBubbleLeftEllipsisIcon, BookmarkIcon, HomeIcon, UserGroupIcon } from '@heroicons/react/24/outline'
+import { useSession } from 'next-auth/react'
+import Image from 'next/image';
 
 function Sidebar() {
+    const { data: session } = useSession();
+
     return (
         <div className='fixed h-full flex flex-col items-start w-[340px] p-2 gap-4'>
-            <UserCircleIcon className='h-10 ml-[6px] mt-2 hover:border-2 hover:border-blue-500 hover:rounded-full cursor-pointer' />
+            <div className='ml-[10px] mt-[20px] cursor-pointer'>
+                <Image
+                    className='rounded-full cursor-pointer'
+                    height={40}
+                    width={40}
+                    src={session.user.image}
+                    alt={session.user.name}
+                    objectFit="cover" />
+            </div>
             <SidebarLink Icon={HomeIcon} text="Home" active />
             <SidebarLink Icon={BellIcon} text="Notification Settings" />
             <SidebarLink Icon={ChatBubbleLeftEllipsisIcon} text="Messages" />
